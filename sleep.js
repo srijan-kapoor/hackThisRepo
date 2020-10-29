9ef6f915210336bf7c579e305aadcbfd83b03675
Harshaan Nihal Khan <harshaan.n.khan@gmail.com>
2020-10-29 23:24:02 +0530

    Create sleep.js (#23)

---
+++ sleep.js
@@ -0,0 +1,9 @@
+// delay execution of function
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+async function sleepyWork() {
+  console.log("I'm going to sleep for 1 second.");
+  await sleep(1000);
+  console.log('I woke up after 1 second.');
+}
